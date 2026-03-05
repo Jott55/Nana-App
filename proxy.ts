510237a0b604309693d278cdaf6fb5ccf4c5a89a
Jott55 <josuhbs@gmail.com>
2026-03-05 11:45:09 -0300

    changed name from middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { verifyTokenAccess } from "./lib/auth";
 import { auth } from "./lib/exports";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
 
     const token = request.cookies.get('accessToken')?.value;
     
